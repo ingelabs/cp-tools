53777c01cb556b339868c4a11e4727ac11e9084c
C. Brian Jones <cbj@gnu.org>
2005-02-17 03:31:36 +0000

    * acinclude.m4: removed * m4: new directory * m4/ac_check_class.m4: new file from gjdoc * m4/ac_check_classpath.m4: ditto * m4/ac_check_home.m4: ditto * m4/ac_check_junit.m4: ditto * m4/ac_check_rqrd_class.m4: ditto * m4/ac_java_options.m4: ditto * m4/ac_prog_antlr.m4: ditto * m4/ac_prog_jar.m4: ditto * m4/ac_prog_java_cc.m4: ditto * m4/ac_prog_javac.m4: ditto * m4/ac_prog_javac_works.m4: ditto * m4/ac_prog_javadoc.m4: ditto * m4/ac_prog_javah.m4: ditto * m4/ac_prog_java.m4: ditto * m4/ac_prog_java_works.m4: ditto * m4/ac_try_run_javac.m4: ditto * m4/am_prog_gcj.m4: ditto * m4/ac_classpath_add.m4: new file * autogen.sh: copied from classpath for version checking

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-AUTOGEN_SUBDIR_MODE=1
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
 cd $srcdir
 PROJECT=cp-tools
-TEST_TYPE=-d
-FILE=src/gnu/classpath/tools
+TEST_TYPE=-f
+FILE=src/gnu/classpath/tools/javap/Javap.java
 
 DIE=0
 
@@ -31,20 +29,42 @@ if $have_libtool ; then : ; else
 	DIE=1
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+have_autoconf=false
+if autoconf --version < /dev/null > /dev/null 2>&1 ; then
+	autoconf_version=`autoconf --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+	case $autoconf_version in
+	    2.59*)
+		have_autoconf=true
+		;;
+	esac
+fi
+if $have_autoconf ; then : ; else
 	echo
-	echo "You must have autoconf installed to compile $PROJECT."
+	echo "You must have autoconf 2.59 installed to compile $PROJECT."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
 	DIE=1
-}
+fi
 
-if automake --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake
-    ACLOCAL=aclocal
-else
+have_automake=false
+# We know each 1.9.x version works
+if automake-1.9 --version < /dev/null > /dev/null 2>&1 ; then
+	AUTOMAKE=automake-1.9
+	ACLOCAL=aclocal-1.9
+	have_automake=true
+elif automake --version < /dev/null > /dev/null 2>&1 ; then
+	AUTOMAKE=automake
+	ACLOCAL=aclocal
+	automake_version=`automake --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+	case $automake_version in
+	    1.9*)
+		have_automake=true
+		;;
+	esac
+fi
+if $have_automake ; then : ; else
 	echo
-	echo "You must have automake 1.9.x installed to compile $PROJECT."
+	echo "You must have automake 1.9 installed to compile $PROJECT."
 	echo "Install the appropriate package for your distribution,"
 	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	DIE=1
@@ -59,22 +79,44 @@ test $TEST_TYPE $FILE || {
 	exit 1
 }
 
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
+if test "x$AUTOGEN_SUBDIR_MODE" = "xyes"; then
         if test -z "$*"; then
                 echo "I am going to run ./configure with no arguments - if you wish "
                 echo "to pass any to it, please specify them on the $0 command line."
         fi
 fi
 
-$ACLOCAL $ACLOCAL_FLAGS -I . || exit $?
+if test -z "$ACLOCAL_FLAGS"; then
+
+	acdir=`$ACLOCAL --print-ac-dir`
+        m4list="glib-2.0.m4 glib-gettext.m4"
+
+	for file in $m4list
+	do
+		if [ ! -f "$acdir/$file" ]; then
+			echo "WARNING: aclocal's directory is $acdir, but..."
+			echo "         no file $acdir/$file"
+			echo "         You may see fatal macro warnings below."
+			echo "         If these files are installed in /some/dir, set the ACLOCAL_FLAGS "
+			echo "         environment variable to \"-I /some/dir\", or install"
+			echo "         $acdir/$file."
+			echo ""
+		fi
+	done
+fi
+
+# Use the "-I ." flag in order to include our pkg.m4.  
+$ACLOCAL -I . -I m4 $ACLOCAL_FLAGS || exit $?
 
 libtoolize --force || exit $?
+
+#autoheader || exit $?
 
 $AUTOMAKE --add-missing || exit $?
 autoconf || exit $?
 cd $ORIGDIR || exit $?
 
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
+if test "x$AUTOGEN_SUBDIR_MODE" = "xyes"; then
         $srcdir/configure --enable-maintainer-mode $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
 
         echo 
